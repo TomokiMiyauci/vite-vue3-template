335a8fb3a414792d0acbda0e2f7f5a6f938a764a
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-10-28 23:57:05 +0900

    :art: Apply prettier

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,15 @@
 <template>
-  <img
-    alt="Vue logo"
-    src="./assets/logo.png"
-  >
+  <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3.0 + Vite" />
 </template>
 
 <script lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+  import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    components: {
+      HelloWorld,
+    },
   }
-}
 </script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,23 +1,21 @@
 <template>
   <h1>{{ msg }}</h1>
-  <button @click="count++">
-    count is: {{ count }}
-  </button>
-  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+  <button @click="count++">count is: {{ count }}</button>
+  <p>
+    Edit <code>components/HelloWorld.vue</code> to test hot module replacement.
+  </p>
 </template>
 
 <script lang="ts">
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: {type: String,
-    default: ''
+  export default {
+    name: 'HelloWorld',
+    props: {
+      msg: { type: String, default: '' },
     },
-  },
-  data: (): {count: number} => {
-    return {
-      count: 0
-    }
+    data: (): { count: number } => {
+      return {
+        count: 0,
+      }
+    },
   }
-}
 </script>
